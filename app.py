5caa0dcc64b715d2898bb123d43d8df2dc6a42d6
a-krawciw <freakyfactoid@gmail.com>
2021-04-28 19:52:35 -0700

    Added html page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from quart import Quart
+from quart import render_template
 
 app = Quart(__name__)
 
+
+@app.route('/')
+async def dashboard():
+    return await render_template("High_Five_Dashboard.html")
 
 @app.route('/')
 def hello_world():
