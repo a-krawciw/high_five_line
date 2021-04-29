7278a9dbb1e63d02ce206b61cb66c82190fb448a
a-krawciw <freakyfactoid@gmail.com>
2021-04-28 22:57:33 -0700

    Allows for deletion of the message.

--- static/main.js
+++ static/main.js
@@ -3,6 +3,7 @@ var ws = new WebSocket('ws://' + document.location.host + document.location.path
 
 function getNewHighFive(name, locX, locY) {
     var elem = "<figure " +
+            "id='" + name + "' " +
             "onclick='returnHighFive(\"" + name + "\")' " +
             "style='position: absolute; left: " + locX + "px; top: " + locY + "px; width: 38px; height: 50px'>" + 
             "<img " + 
@@ -14,20 +15,36 @@ function getNewHighFive(name, locX, locY) {
     return elem
 }
 
+function getNewSentHighFive(name, locX, locY) {
+    var elem = "<figure " +
+            "id='" + name + "' " +
+            "style='position: absolute; left: " + locX + "px; top: " + locY + "px; width: 38px; height: 50px'>" +
+            "<img " +
+            "src='/static/images/hand_icon.png' width='38px'; height='50px' " +
+            "/>" +
+            "<figcaption>" + name + "</figcaption>" +
+            "</figure>";
+
+    return elem
+}
+
+
+
 ws.onmessage = function (event) {
     console.log(event.data);
     message = JSON.parse(event.data)
 
     if (message.type === "banner") {
         $("#banner").html(message.payload);
-    }
-
-    if (document.location.pathname.includes(message.target)){
-        highFive(message);
-    }
-
-    else if (document.location.pathname.includes(message.sender)) {
-        console.log("You send a message to " + message.target)
+    } else if (message.type === "create") {
+        if (document.location.pathname.includes(message.target)) {
+            highFive(message);
+        } else if (document.location.pathname.includes(message.sender)) {
+            sentHighFive(message);
+        }
+    } else if (message.type === "accept") {
+        removeHighFive(message.target);
+        removeHighFive(message.sender);
     }
 };
 
@@ -38,7 +55,23 @@ function highFive(message) {
     $("#main-canvas").append(getNewHighFive(message.sender, randX, randY));
 }
 
+function sentHighFive(message) {
+    randX = Math.floor(Math.random() * ($("#main-canvas").width() - 38));
+    randY = Math.floor(Math.random() * ($("#main-canvas").height() - 50));
+
+    $("#main-canvas").append(getNewSentHighFive(message.target, randX, randY));
+}
+
+function removeHighFive(id) {
+    $("#" + id).remove();
+}
+
 function createHighFive(target_name){
     message = {type: "create", target: target_name}
     ws.send(JSON.stringify(message));
+}
+
+function returnHighFive(sender_name) {
+    message = {type: "accept",  target: sender_name}
+    ws.send(JSON.stringify(message))
 }
